f77ea74b0d3c128653c49f67ae3932eb295db7dc
asd1495 <armando.salcedo95@gmail.com>
2023-05-08 15:34:56 -0600

    added assets folder

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import logoDark from '../assets/LogoHappenOscuro.png';
+import logoPngDark from '../assets/Logotransparente.png';
+import logoPngLight from '../assets/Logotransprenteblanco.png';
+import lonaLogo from '../assets/LONALOGO.jpg';
+
+export default {
+    logoDark,
+    logoPngDark,
+    logoPngLight,
+    lonaLogo
+};
